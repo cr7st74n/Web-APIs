e11920dc73aad2f96bec5835c771a32702463d66
cr7st74n <cris-082011@hotmail.com>
2022-06-30 22:09:51 -0500

    update

--- JS/web.js
+++ JS/web.js
@@ -1,6 +1,13 @@
 
-var startBtn = document.querySelector("#start");
-var Button1 = document.querySelector("#opt1");
-var Button2 = document.querySelector("#opt2");
-var Button3 = document.querySelector("#opt3");
-var Button4 = document.querySelector("#opt4");
+var start = document.getElementById("StartPage");
+var startBtn = document.getElementById("start");
+var getQuestion = document.getElementById("TestPage");
+
+
+startBtn.addEventListener("click", pageQuestions);
+
+function pageQuestions(){
+    console.log("this is working");
+    start.classList.add("hide");
+    getQuestion.classList.remove("hide");
+}
