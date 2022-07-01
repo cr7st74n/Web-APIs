b2233e8077e3e31a2034a5c87ef68d2a2141ef39
cr7st74n <cris-082011@hotmail.com>
2022-07-01 09:30:47 -0500

    update

--- JS/web.js
+++ JS/web.js
@@ -1,11 +1,19 @@
 
 var start = document.getElementById("StartPage");
+var continua = document.getElementById("ContButton");
+
 var startBtn = document.getElementById("start");
 var getQuestion = document.getElementById("TestPage");
 var timeLe = document.getElementById("timer");
 
 var questionbeg = document.getElementById("question");
-var questionBox = document.querySelector("TestPage");
+var questionBox = document.getElementById("TestPage");
+var correctOrIncorrect = document.querySelector("check");
+
+var option1 = document.getElementById("opt1");
+var option2 = document.getElementById("opt2");
+var option3 = document.getElementById("opt3");
+var option4 = document.getElementById("opt4");
 
 
 
@@ -34,22 +42,49 @@ function pageQuestions(){
     getQuestion.classList.remove("hide");
     startQuestions = 0;
     posQuestions = JSquestions;
-    Questions();
     countDown();
+    Questions();
+
 }
 
 function Questions(){
+    startAgain();
     jumpQuestion(posQuestions[startQuestions]);
-
 }
+
+
+
+
 
 function jumpQuestion(question){
     questionbeg.innerText = question.question;
+
+    question.answers.forEach(answer => {
+        var button = document.createElement("button");
+        button.classList.add("btn");
+        button.innerText = answer.text;
+        if (answer.correct){
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click",answers);
+        questionBox.appendChild(button);
+        
+    });
+}
+
+function startAgain() {
+    continua.classList.add("hide");
+    while(questionBox.firstChild){
+        questionBox.removeChild(questionBox.firstChild);
+    }
+
 }
 
 
-function answers(){
-
+function answers(obj){
+    var selectButton = obj.target;
+    var correct = selectButton.dataset.correct;
+    
 }
 
 var JSquestions = [
@@ -74,4 +109,3 @@ var JSquestions = [
     }
 ]
 
-//"<script>","<scripting>","<body>","<html>"
