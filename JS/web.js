2ce411c8d0505f6e19956f4c9f2f02eb050f1450
cr7st74n <cris-082011@hotmail.com>
2022-06-30 20:12:34 -0500

    update form PC

---
+++ JS/web.js
@@ -0,0 +1,6 @@
+
+var startBtn = document.querySelector("#start");
+var Button1 = document.querySelector("#opt1");
+var Button2 = document.querySelector("#opt2");
+var Button3 = document.querySelector("#opt3");
+var Button4 = document.querySelector("#opt4");
